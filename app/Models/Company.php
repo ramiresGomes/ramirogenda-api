e468478bf7fd462575f962d19bb8c205733fe673
marcos-dev14 <mprogramador.10@gmail.com>
2024-06-08 19:19:10 -0300

    feat: implementação do model Company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\softDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    use HasFactory;
+    use softDeletes;
+
+    protected $fillable = [
+        'name',
+        'full_address',
+        'phone',
+        'email',
+        'password'
+    ];
 }
